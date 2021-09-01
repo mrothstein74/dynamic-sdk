80bd64e123418284907bdb08a15340a0a8f62c08
Michael Rothstein <michaelrothstein@Michaels-MacBook-Pro.local>
2021-08-31 17:07:58 -0700

    checking in

---
+++ .circleci/dynamic/jobs/jobB.js
@@ -0,0 +1,8 @@
+const CircleCI = require("@circleci/circleci-config-sdk");
+const dockerNode = require("../executors/docker-node");
+
+const jobB = new CircleCI.Job("jobB", dockerNode);
+
+jobA.addStep(new CircleCI.Command.Run({ command: "echo hello from Job B" }));
+
+module.exports = jobB;
